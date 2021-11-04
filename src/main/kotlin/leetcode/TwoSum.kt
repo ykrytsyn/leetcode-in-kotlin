2d8e5f099bd0ba3b919fe7a2238fb1446e50494f
Yury Krytsyn <17603645+ykrytsyn@users.noreply.github.com>
2021-11-03 22:58:46 -0700

    LC #1: TwoSum

---
+++ src/main/kotlin/leetcode/TwoSum.kt
@@ -0,0 +1,64 @@
+package leetcode
+
+
+/**
+ * # Problem #1
+ * [https://leetcode.com/problems/two-sum/](https://leetcode.com/problems/two-sum/)
+ *
+ * ### Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * ### You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * ### You can return the answer in any order.
+ *
+ */
+class TwoSum {
+
+    /**
+     * Brute Force
+     */
+    fun bruteForce(nums: IntArray, target: Int): IntArray? {
+        for (i in nums.indices) {
+            for (j in i + 1 until nums.size) {
+                if (nums[j] == target - nums[i]) {
+                    return intArrayOf(i, j)
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null
+    }
+
+    /**
+     * Two-pass Hash Table
+     */
+    fun twoPassHashTable(nums: IntArray, target: Int): IntArray? {
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            map[nums[i]] = i
+        }
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement) && map[complement] != i) {
+                return intArrayOf(i, map[complement]!!)
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null
+    }
+
+    /**
+     * One-pass Hash Table
+     */
+    fun onePassHashTable(nums: IntArray, target: Int): IntArray? {
+        val map: MutableMap<Int, Int> = HashMap()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
+            }
+            map[nums[i]] = i
+        }
+        // In case there is no solution, we'll just return null
+        return null
+    }
+
+}
